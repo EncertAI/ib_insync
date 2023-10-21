98b8791cf4e6c529935791cf2cdbea551cb59714
jafonollosa <jafonollosa@encert.ai>
2023-10-21 17:40:17 +0000

    modify reqId on recorded responses

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ import base64
 from collections import defaultdict
 import json
 import pathlib
+import struct
+from typing import Optional
 
 import pytest
 
@@ -13,29 +15,55 @@ class JsonReader:
     def __init__(self, json_file: str):
         with open(json_file) as f:
             self.request_response = json.load(f)
-        self.event = asyncio.Event()
+        self.queue = asyncio.Queue()
+        self.response = None
 
-    def write(self, request: bytes):
-        encoded_request = base64.b64encode(request).decode('utf-8')
-        if encoded_request not in self.request_response:
-            raise ValueError(f"Unknown request: {request} {encoded_request}")
-        self.response = self.request_response[encoded_request]
-        self.index = 0
-        self.event.set()
+    def write(self, encoded_request: str, reqId: Optional[str] = None):
+        self.queue.put_nowait((encoded_request, reqId))
 
     async def read(self, n: int = -1) -> bytes:
-        await self.event.wait()
+        if self.response is None:
+            encoded_request, reqId = await self.queue.get()
+            if encoded_request not in self.request_response:
+                print(f"request not found: {encoded_request}")
+                self.response = []
+            else:
+                self.response = self.request_response[encoded_request]
+                self.reqId = reqId
+            self.index = 0
         encoded_data = self.response[self.index]
         data = base64.b64decode(encoded_data)
+        if self.reqId is not None:
+            fields = data[4:].decode(errors='backslashreplace').split('\0')
+            if fields[0] != '8':
+                fields[2] = self.reqId
+                data = data[:4] + ('\0'.join(fields)).encode()
         self.index += 1
         if self.index == len(self.response):
-            self.event.clear()
+            self.response = None
         return data
 
     def close(self):
-        self.response = [""]
-        self.index = 0
-        self.event.set()
+        self.queue
+        self.response = None
+
+
+def get_fields(buffer: bytes) -> list[str]:
+    fields = []
+    data = b""
+    msgEnd = 0
+
+    # 4 byte prefix tells the message length
+    if len(buffer) >= 4:
+        msgEnd = 4 + struct.unpack('>I', buffer[:4])[0]
+
+    if msgEnd and len(buffer) >= msgEnd:
+        data = buffer[:msgEnd]
+        msg = data[4:].decode(errors='backslashreplace')
+        fields = msg.split('\0')[:-1]
+        buffer = buffer[msgEnd:]
+
+    return fields, buffer, data
 
 
 class TwsStub:
@@ -72,26 +100,53 @@ class TwsStub:
         )
 
     async def response_reader(self, tws_reader, writer):
+        buffer = b""
         while True:
             data = await tws_reader.read(1 << 12)
             if not data:
                 break
-            if not self.stub:
-                encoded_data = base64.b64encode(data).decode('utf-8')
-                self.request_response[self.current_request].append(encoded_data)
-                print(".", end="")
-            writer.write(data)
-        tws_reader.close()
+
+            buffer += data
+            while buffer:
+                fields, buffer, data = get_fields(buffer)
+                if not fields:
+                    break
+                print(f"TwsStub response: {fields}")
+                if not self.stub:
+                    encoded_data = base64.b64encode(data).decode()
+                    self.request_response[self.current_request].append(encoded_data)
+                writer.write(data)
 
     async def request_writer(self, reader, tws_writer):
+        first_request = True
+        buffer = b""
         while True:
             data = await reader.read(1 << 12)
             if not data:
                 break
-            encoded_data = base64.b64encode(data).decode('utf-8')
-            print(f"\nTwsStub request: {data[:32]} {encoded_data[:128]}")
-            self.current_request = encoded_data
-            tws_writer.write(data)
+
+            buffer += data
+            while buffer:
+                if first_request:
+                    msg = buffer.decode(errors='backslashreplace')
+                    fields = msg.split('\0')
+                    first_request = False
+                    buffer = b""
+                else:
+                    fields, buffer, data = get_fields(buffer)
+                if not fields:
+                    break
+                encoded_request = fields[0] + '_' + fields[1]
+                self.current_request = encoded_request
+                print(f"\nTwsStub request: {fields} {data}")
+                if not self.stub:
+                    tws_writer.write(data)
+                    await asyncio.sleep(0.1)
+                else:
+                    code = int(fields[0]) if fields[0].isdigit() else 0
+                    reqId = fields[2] if code in [7, 62, 76] else None
+                    tws_writer.write(encoded_request, reqId)
+
         tws_writer.close()
 
     def close(self):
@@ -117,6 +172,20 @@ async def ib():
     ib = ibi.IB()
     await ib.connectAsync()
     yield ib
-    ib.disconnect()
+    await ib.disconnectAsync()
     tws.close()
     await tws.wait_closed()
+
+
+if __name__ == '__main__':
+    async def main():
+        tws = await TwsStub().start()
+        ib = ibi.IB()
+        await ib.connectAsync()
+        summary = await ib.accountSummaryAsync()
+        assert summary
+        await ib.disconnectAsync()
+        tws.close()
+        await tws.wait_closed()
+
+    asyncio.run(main())
